d3e7f6ef959efa247ccf17ba78ee7da685f7934b
ryota.yamauchi <ryota.y0128@gmail.com>
2020-07-30 02:25:48 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,7 +3,7 @@ class ToppagesController < ApplicationController
     if logged_in?
       @micropost = current_user.microposts.build
       # 新規投稿用 form_with用
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end 
   end
 end
